5ba688d1edd0dfc58cd58765b8f218292042add8
Bryson-Mazzarello <bryson.mazzarello@stu.ocsb.ca>
2024-10-24 09:58:32 -0400

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,1 +1,11 @@
-
+Dtr = float(input())
+if not Dtr %1 == 0:
+print(“Invalid Character”)
+else:
+Rad = d/2
+Circ = 3.14*r*2
+Area = r*r*3.14
+print(“Diameter inputed”, Dtr)
+print(“Radius inputed”, Rad)
+print(“Circumference”, Circ)
+print(“Area”, Area)
